6b280229ab60856dc9d55bd6ac428a0dbefa681a
Giovanno98 <vanno.gio1998@gmail.com>
2016-10-12 06:41:37 -0700

    Studi kasus 5
    dalam studi kasus 5 ini ada soal:
    1. Studi_Kasus5_1
    2. Studi_Kasus5_2
    3. Studi_Kasus5_3
    4. Studi_Kasus5_4

---
+++ Studi_kasus5.2.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Study kasus 5.1</title>
+</head>
+<body>
+<?php
+	$toko = array
+  (
+  array("kode barang","nama barang","harga satuan"),
+  array(1001,"Sabun Lifeboy","Rp. 1500"),
+  array(1002,"Permen Blaster","Rp. 5600"),
+  array(1003,"Pasta Gigi Pepsodent","Rp. 4560"),
+  array(1004,"Madu Arbain","Rp. 30000"),
+  array(1005,"Kecap ABC","Rp. 7250"),
+  array(1006,"Saus Tomat ABC","Rp. 6700"),
+  array(1007,"Gula Gulaku","Rp. 8900"),
+  array(1008,"Rinso","Rp. 7100"),
+  array(1009,"Super Pel","Rp. 6450"),
+  array(1010,"Permen Tango","Rp. 5600")
+  );
+	echo $toko[0][0]."     ".$toko[0][1]."     ".$toko[0][2].".<br>";
+  echo $toko[1][0]."     ".$toko[1][1]."     ".$toko[1][2].".<br>";
+  echo $toko[2][0]."     ".$toko[2][1]."     ".$toko[2][2].".<br>";
+  echo $toko[3][0]."     ".$toko[3][1]."     ".$toko[3][2].".<br>";
+  echo $toko[4][0]."     ".$toko[4][1]."     ".$toko[4][2].".<br>";
+  echo $toko[5][0]."     ".$toko[5][1]."     ".$toko[5][2].".<br>";
+  echo $toko[6][0]."     ".$toko[6][1]."     ".$toko[6][2].".<br>";
+  echo $toko[7][0]."     ".$toko[7][1]."     ".$toko[7][2].".<br>";
+  echo $toko[8][0]."     ".$toko[8][1]."     ".$toko[8][2].".<br>";
+  echo $toko[9][0]."     ".$toko[9][1]."     ".$toko[9][2].".<br>";
+?>
+</body>
+</html>
---
+++ Studi_kasus5_3.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Study kasus 5.3</title>
+</head>
+<body>
+<?php
+	$bil = array
+  (
+  array(5,10,15,20,25,30,35,40),
+  array(6,11,16,21,26,31,36,41),
+  array(7,12,17,22,27,32,37,42),
+  array(8,13,18,23,26,33,38,43),
+
+  );
+?>
+</body>
+</html>
